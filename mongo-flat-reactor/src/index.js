8e7162ae5f642a7b37919c403e32f8ab9bcfb7e8
Joshua Saxton <saxtonjoshua@live.com>
2018-02-11 16:52:54 -0500

    Added launch.json
    Added launch.json to configure debugging in VS Code

--- mongo-flat-reactor/src/index.js
+++ mongo-flat-reactor/src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
